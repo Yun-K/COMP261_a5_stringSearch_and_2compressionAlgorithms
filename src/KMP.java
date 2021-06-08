035b233eba2b28fe649a0c78da58d7800c9f345b
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-06-08 17:14:29 +0800

    implement the jump table methopd'

--- src/KMP.java
+++ src/KMP.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class KMP {
+    static String[] args = new String[] { "data/war_and_peace.txt", "astronomy" };
 
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
         if (args.length != 2) {
-            System.out.println("Please call this program with " +
-                               "two arguments which is the input file name " +
-                               "and the string to search.");
+            System.out.println("Please call this program with " + "two arguments which is the input file name "
+                    + "and the string to search.");
         } else {
             try {
                 Scanner s = new Scanner(new File(args[0]));
@@ -33,6 +33,28 @@ public class KMP {
      */
     public static int search(String text, String pattern) {
         // TODO
+        int[] jumpTable = getJumpTable(pattern);
+
         return -1;
+    }
+
+    public static int[] getJumpTable(String pattern) {
+        int size = pattern.length();
+        int[] jumpTableToReturn = new int[size];
+        jumpTableToReturn[0] = -1;
+        jumpTableToReturn[1] = 0;
+        int pos = 2, j = 0;
+        while (pos < size) {
+            // substrings ...pos-1 and 0..j match
+            if (pattern.charAt(pos - 1) == pattern.charAt(j)) {
+                jumpTableToReturn[pos++] = ++j;
+            } else if (j > 0) {// mismacth, restart the prefix
+                j = jumpTableToReturn[j];
+            } else {// j=0 //we have run out of candidate prefixes
+                jumpTableToReturn[pos++] = 0;
+            }
+        }
+
+        return jumpTableToReturn;
     }
 }
